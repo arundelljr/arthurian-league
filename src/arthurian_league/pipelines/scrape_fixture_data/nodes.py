bfeb86013827b7d30e4a5ee7b44a1d6a2f421e38
John Arundell <arundelljr@gmail.com>
2025-01-14 08:50:00 +0000

    Updating scraping logic

--- src/arthurian_league/pipelines/scrape_fixture_data/nodes.py
+++ src/arthurian_league/pipelines/scrape_fixture_data/nodes.py
@@ -1,35 +1,75 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import json
-from kedro_datasets.json import JSONDataset
 from arthurian_league.pipelines.save_season_ids.nodes import _get_url_soup
 
+# Set up of functions to be used when extracting information from fixture results table
 
-def _extract_html_attribute(loop_item, html_tag1, html_class1, html_tag2=None, html_class2=None):
-   # Find the primary tag and class
-    primary_element = loop_item.find(html_tag1, class_=html_class1)
+# Explore table
+table = soup.find(class_="tbody")
 
-    # If a second tag is specified, find it inside the primary element
-    if html_tag2:
-        if html_class2:
-            secondary_element = primary_element.find(html_tag2, class_=html_class2)
-        else:
-            secondary_element = primary_element.find(html_tag2)
-        return secondary_element.text.strip()
-    # If no second tag, extract from the primary element
-    return primary_element.text.strip()
+# Find and extract fixture ids
+_list = table.find_all()
+
+fixture_ids = []
+
+for fixture in _list:
+    if fixture.get('id') is not None:
+        ids = fixture.get('id').split("fixture-")[1]
+        fixture_ids.append(ids)
 
 
-# Loop through each season and scrape data
-def _scrape_fixture_data(seasons):
-    all_data = []
-    for season_id, season_name in seasons.items():
-        print(f"Scraping season: {season_name}")  # Print season being scraped
+def _extract_element(table, secondary_tag, primary_tag="div"):
+    data = [] # Create empty list
+    _list = table.find_all(primary_tag, class_=secondary_tag) # Find element within soup
+
+    for element in _list:
+        fixture_elements = element.text.strip()
+        data.append(fixture_elements)
+    return data
+
+
+def _exctract_fixture_data(seasons):
     
+    all_data= [] # Create empty list
+    
+    for season_name, season_id in seasons:
+        print(f"Scraping season: {season_name}")  # Print season being scraped
+        
+        # Use dynamic URL to cycle through seasons
         soup = _get_url_soup(f"https://fulltime.thefa.com/results/1/10000.html?selectedSeason={season_id}&selectedFixtureGroupAgeGroup=0&previousSelectedFixtureGroupAgeGroup=&selectedFixtureGroupKey=&previousSelectedFixtureGroupKey=&selectedDateCode=all&selectedRelatedFixtureOption=2&selectedClub=&previousSelectedClub=&selectedTeam=")
         
-        # Find all fixtures inside the 'tbody' class
+        # Find fixture table
+        table = soup.find_all("div", class_="tbody")
+
+        for fixture in table:
+            fixture_id =
+            home_team = "home
+            score = _extract_element(table, "score-col")
+            
+            
+        
+
+
+
+
+
+
+
+
+
+# Loop through each season and scrape fixture data
+def _scrape_fixture_data(seasons):
+    
+    all_data = [] # Create empty list
+    
+    for season_id, season_name in seasons.items():
+        print(f"Scraping season: {season_name}")  # Print season being scraped
+        
+        # Use dynamic URLs to cycle through seasons
+        soup = _get_url_soup(f"https://fulltime.thefa.com/results/1/10000.html?selectedSeason={season_id}&selectedFixtureGroupAgeGroup=0&previousSelectedFixtureGroupAgeGroup=&selectedFixtureGroupKey=&previousSelectedFixtureGroupKey=&selectedDateCode=all&selectedRelatedFixtureOption=2&selectedClub=&previousSelectedClub=&selectedTeam=") 
+        
+        # 
         fixtures = soup.find_all("div", class_="tbody")[0].find_all("div", id=lambda x: x and x.startswith("fixture-"))
     
         # Extract competition, date, home team, away team, score for each fixture
@@ -47,6 +87,39 @@ def _scrape_fixture_data(seasons):
     # Store data in a pandas DataFrame
     fixtures_df = pd.DataFrame(all_data, columns=["Fixture_ID", "Season", "Competition", "Date", "Home_Team", "Away_Team", "Score"]) 
     return fixtures_df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Extract 
+def _extract_html_attribute(item, tag1, class1, tag2=None, class2=None):
+   
+    # Find the primary tag and class
+    primary_element = item.find(tag1, class_=class1)
+
+    # If a second tag and class are specified, find nested secondary element
+    if tag2:
+        if class2:
+            secondary_element = primary_element.find(tag2, class_=class2)
+        else:
+            secondary_element = primary_element.find(tag2)
+        # Extract from secondary element
+        return secondary_element.text.strip()
+    # If no second tag or class, extract from the primary element
+    return primary_element.text.strip()
+
+
+
 
 def _save_as_csv(df: pd.DataFrame, save_filepath: str):
     df.to_csv(save_filepath)
